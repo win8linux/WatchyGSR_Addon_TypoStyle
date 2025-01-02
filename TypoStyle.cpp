52f952d54074e160bf235d39ac4d7fc49a4f4884
Julius Enriquez <win8linux@gmail.com>
2025-01-03 01:37:30 +0800

    Initial port

---
+++ TypoStyle.cpp
@@ -0,0 +1,132 @@
+#include "TypoStyle.h"
+
+WatchyGSR watchyGSR;
+
+#define GREY 0x7BEF
+
+void drawPixel(int16_t x, int16_t y, uint16_t col)
+{
+    switch (col) {
+    case GREY:
+        if (y & 1) {
+            if (x & 1) {
+                WatchyGSR::display.drawPixel(x, y, watchyGSR.BackColor());
+            } else {
+                WatchyGSR::display.drawPixel(x, y, watchyGSR.ForeColor());
+            }
+        } else {
+            if (x & 1) {
+                WatchyGSR::display.drawPixel(x, y, watchyGSR.ForeColor());
+            } else {
+                WatchyGSR::display.drawPixel(x, y, watchyGSR.BackColor());
+            }
+        }
+        break;
+    default:
+        WatchyGSR::display.drawPixel(x, y, col);
+        break;
+    }
+}
+
+void drawBitmapCol(int16_t x, int16_t y, const uint8_t* bitmap, int16_t w, int16_t h, uint16_t color1, uint16_t color2)
+{
+    int16_t i, j, byteWidth = (w + 7) / 8;
+    for (j = 0; j < h; j++) {
+        for (i = 0; i < w; i++) {
+            if ((pgm_read_byte(bitmap + j * byteWidth + i / 8) & (128 >> (i & 7)))) {
+                drawPixel(x + i, y + j, color1);
+            } else {
+                drawPixel(x + i, y + j, color2);
+            }
+        }
+    }
+}
+
+void drawMyRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color1)
+{
+    for (int j = 0; j < h; j++) {
+        for (int i = 0; i < w; i++) {
+            drawPixel(x + i, y + j, color1);
+        }
+    }
+}
+
+void drawWatchFace()
+{ // override this method to customize how the watch face looks
+
+    int clockLeft = -14;
+    int clockTop = 15;
+    int hour1 = WatchTime.Local.Hour / 10;
+    int hour2 = WatchTime.Local.Hour % 10;
+    int minute1 = WatchTime.Local.Minute / 10;
+    int minute2 = WatchTime.Local.Minute % 10;
+    int month = WatchTime.Local.Month - 1;
+    int day1 = WatchTime.Local.Day / 10;
+    int day2 = WatchTime.Local.Day % 10;
+    int year = WatchTime.Local.Year + 1970;
+    int weekday = WatchTime.Local.Wday - 1;
+    int year1, year2, year3, year4;
+
+    year4 = year % 10;
+    year /= 10;
+    year3 = year % 10;
+    year /= 10;
+    year2 = year % 10;
+    year /= 10;
+    year1 = year % 10;
+    year /= 10;
+
+    // int col1 = GREY; // foreground colour
+    int col1 = watchyGSR.ForeColor(); // foreground colour
+    int col2 = watchyGSR.BackColor(); // background colour
+
+    // drawbg
+    drawMyRect(0, 0, 200, 200, col2);
+
+    // time
+    int ds = sizeof(clockDigits) / 10; // 10 digits in image, so each frame is size of array/10
+    int glyphWidth = 54;
+    int glyphHeight = 72;
+    col1 = watchyGSR.ForeColor();
+    drawBitmapCol(clockLeft, clockTop, &clockDigits[hour1 * ds], glyphWidth, glyphHeight, col1, col2);
+    drawBitmapCol(clockLeft + glyphWidth, clockTop, &clockDigits[hour2 * ds], glyphWidth, glyphHeight, col1, col2);
+    col1 = GREY;
+    drawBitmapCol(clockLeft + (glyphWidth * 2) + (glyphWidth * 0.25), clockTop, &clockDigits[minute1 * ds], glyphWidth, glyphHeight, col1, col2);
+    drawBitmapCol(clockLeft + (glyphWidth * 3) + (glyphWidth * 0.25), clockTop, &clockDigits[minute2 * ds], glyphWidth, glyphHeight, col1, col2);
+
+    drawMyRect(clockLeft + (glyphWidth * 2) + 2, clockTop + 20, 10, 10, col1);
+    drawMyRect(clockLeft + (glyphWidth * 2) + 2, clockTop + 40, 10, 10, col1);
+
+    // month
+    ds = sizeof(monthName) / 12;
+    col1 = watchyGSR.ForeColor();
+    drawBitmapCol(clockLeft - 9, clockTop + 72, &monthName[month * ds], 250, 30, col1, col2);
+
+    // day
+    ds = sizeof(dateDigits) / 10;
+    glyphWidth = 45;
+    glyphHeight = 60;
+    col1 = GREY;
+    drawBitmapCol(clockLeft + 16, clockTop + 107, &dateDigits[day1 * ds], glyphWidth, glyphHeight, col1, col2);
+    drawBitmapCol(clockLeft + 16 + (glyphWidth), clockTop + 107, &dateDigits[day2 * ds], glyphWidth, glyphHeight, col1, col2);
+
+    // weekday
+    ds = sizeof(dayName) / 7;
+    col1 = GREY;
+    drawBitmapCol(clockLeft + 103, clockTop + 105, &dayName[weekday * ds], 154, 17, col1, col2);
+
+    // year
+    ds = sizeof(yearDigits) / 10;
+    glyphWidth = 30;
+    glyphHeight = 45;
+    col1 = watchyGSR.ForeColor();
+    drawBitmapCol(clockLeft + 103, clockTop + 122, &yearDigits[year1 * ds], glyphWidth, glyphHeight, col1, col2);
+    drawBitmapCol(clockLeft + 103 + (glyphWidth), clockTop + 122, &yearDigits[year2 * ds], glyphWidth, glyphHeight, col1, col2);
+    drawBitmapCol(clockLeft + 103 + (glyphWidth * 2), clockTop + 122, &yearDigits[year3 * ds], glyphWidth, glyphHeight, col1, col2);
+    drawBitmapCol(clockLeft + 103 + (glyphWidth * 3), clockTop + 122, &yearDigits[year4 * ds], glyphWidth, glyphHeight, col1, col2);
+
+    // battery
+    float powerPercent = (getBatteryVoltage() - 3.8) * 200;
+    drawMyRect(0, 187, (int)powerPercent, 13, GREY);
+
+} // end function
---
+++ TypoStyle.h
@@ -0,0 +1,15 @@
+#ifndef WATCHY_TYPOSTYLE_H
+#define WATCHY_TYPOSTYLE_H
+#include "GxEPD2.h"
+#include "Watchy_GSR.h"
+#include "graphics.h"
+
+class TypoStyle : public WatchyGSR {
+public:
+    void drawPixel(int16_t x, int16_t y, uint16_t col);
+    void drawBitmapCol(int16_t x, int16_t y, const uint8_t* bitmap, int16_t w, int16_t h, uint16_t color1, uint16_t color2);
+    void drawMyRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color1);
+    void drawWatchFace();
+};
+
+#endif
